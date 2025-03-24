077b6e82ce2794c331f90b9bed199227db526336
sanjivthapa <thapasvt12@gmail.com>
2025-03-24 16:21:42 +0545

    Created some directry and app for frontend

---
+++ frontend/src/app/about/page.js
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+    return (
+      <div style={{ padding: '1rem' }}>
+        <h2>About</h2>
+        <p>I love writing with Next.js!</p>
+      </div>
+    );
+  }
+  
