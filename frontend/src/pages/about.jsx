57f4aa303190ff3e2d245a06fcb9b4303586eedd
sanjivthapa <thapasvt12@gmail.com>
2025-03-28 13:06:44 +0545

    some minor changes

--- frontend/src/pages/about.jsx
+++ frontend/src/pages/about.jsx
@@ -30,6 +30,15 @@ export default function About() {
           
           <p className="mt-4 text-gray-300 mt-6">Will write later lol</p>
         </div>
+        <div className="mt-20 text-left">
+      <h2 className="text-4xl font-bold">GitHub Streak</h2>
+      <img 
+        src="https://camo.githubusercontent.com/9a0faded4de5a37cb65f732079d22def1f1e3e66b54295763581470b08addb36/68747470733a2f2f6e69727a616b2d73747265616b2d73746174732e76657263656c2e6170702f3f757365723d73616e6a69767468617061737674267468656d653d67727576626f7826686964655f626f726465723d66616c7365" 
+        alt="GitHub Streak"
+        className="mt-10"
+        style={{ maxWidth: "100%", height: "auto" }}
+      />
+    </div>
       </div>
     </div>
   );
