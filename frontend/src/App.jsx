ca05f1448352bbb5090be39287cc44d550ec871d
sanjivthapa <thapasvt12@gmail.com>
2025-03-28 12:59:21 +0545

    some minor changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, useLocation, Navigate } from "react-router-dom";
 import Auth from "./pages/auth";
 import SideNavigation from "./components/SideNav";
 import Home from "./pages/Home";
@@ -9,15 +9,21 @@ function Layout() {
   const location = useLocation();
   const showSidebar =! location.pathname.startsWith("/auth");
   
+  function NotFoundRedirect() {
+    return <Navigate to="/home" />;
+  }
+  
   return (
     <div className="flex">
       {showSidebar && <SideNavigation />}
       <div className={`flex-1 ${showSidebar ? 'ml-16 md:ml-56' : ''}`}>
         <Routes>
           <Route path="/auth" element={<Auth />} />
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/posts/:id" element={<PostDetail />} />
           <Route path="/about" element={<About />} />
+          {/* Redirect unknown routes to /home */}
+        <Route path="/" element={<NotFoundRedirect />} />
         </Routes>
       </div>
     </div>
