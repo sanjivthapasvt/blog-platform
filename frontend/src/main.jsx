56d9a0564befec2d752f68dd7ede3c18eed1dfa6
sanjivthapa <thapasvt12@gmail.com>
2025-04-02 19:31:49 +0545

    added views count

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 import { inject } from '@vercel/analytics';
-createRoot(document.getElementById('root')).render(
+
+const root = createRoot(document.getElementById('root'));
+root.render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
+
 inject();
